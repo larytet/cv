e31fd21afcbf6358a13f735ec62ce3142c567108
larytet <>
2024-10-23 16:09:06 +0300

    Add long_version

--- cv.py
+++ cv.py
@@ -4,6 +4,7 @@ from docx2pdf import convert
 
 ADD_TECHOLOGIES = True
 IS_BACKEND = True
+LONG_VERSION = False
 
 # Create a new Document with the updated order
 doc = Document()
@@ -15,19 +16,19 @@ doc.add_paragraph('(054) 4681517 | arkady.miasnikov@gmail.com')
 doc.add_heading('Software Engineer experienced in leading development projects from inception to full implementation', level=1)
 if IS_BACKEND:
     doc.add_paragraph(
-        "Extensive experience in designing and developing mission-critical backend systems, automated trading systems, and multi-threaded applications."
-        "Proficient in optimizing system performance, low-level programming, and integrating hardware with software."
-        "Strong expertise in reverse engineering, kernel-level development, and debugging complex backend systems."
-        "Technology-focused with a broad business perspective, and exceptional problem-solving abilities"
-        "Tools & Technologies: Linux, Go, Python, C, AWS, k8s, SQL" 
+        "Extensive experience in designing and developing mission-critical backend systems, automated trading systems, and multi-threaded applications. "
+        "Proficient in optimizing system performance, low-level programming, and integrating hardware with software. "
+        "Strong expertise in reverse engineering, kernel-level development, and debugging complex backend systems. "
+        "Technology-focused with a broad business perspective, and exceptional problem-solving abilities. "
+        "Tools & Technologies: Linux, Go, Python, C, AWS, k8s, SQL. " 
     )
 else:
     doc.add_paragraph(
-        "Expertise in design and development of mission-critical real-time systems, firmware, embedded systems, multi-threaded applications, automated trading platforms."
-        "Extensive experience in low-level programming, system performance optimization, and hardware-software integration."
-        "Strong background in reverse engineering, kernel development, and debugging across diverse platforms."
-        "Technology-savvy with a broad business perspective, excellent problem-solving skills, and high attention to detail."
-        "Tools & Technologies: Linux, Go, Python, C, C++, Assembly, RTOS, embedded architectures."
+        "Expertise in design and development of mission-critical real-time systems, firmware, embedded systems, multi-threaded applications, automated trading platforms. "
+        "Extensive experience in low-level programming, system performance optimization, and hardware-software integration. "
+        "Strong background in reverse engineering, kernel development, and debugging across diverse platforms. "
+        "Technology-savvy with a broad business perspective, excellent problem-solving skills, and high attention to detail. "
+        "Tools & Technologies: Linux, Go, Python, C, C++, Assembly, RTOS, embedded architectures. "
     )
 
 doc.add_heading('Professional Experience', level=1)
@@ -68,22 +69,27 @@ experience = [
         "Led a streamlining process that improved productivity by 30% by advocating the replacement of ClearCase with GIT.",
         "Established a wiki-based search system that improved access to the company's knowledge base."
     ], ['C/C++', 'Python', "Linux kernel", "ASIC", "Bare metal"], False),
-    # ("2005-2007: Software Engineer - Broadlight Ltd.", [
-    #     "Contributed to the development of the world's first fully integrated GPON chip.",
-    #     "Supported the hardware team in post-production verification and pre-fab processes.",
-    #     "Prepared firmware for design verification, post-production chip verification, and a series of drivers."
-    # ], ['C', 'Verilog', "Linux kernel", "Specman", "Bare metal"], True),
-    # ("2001-2004: Software Engineer - Terayon Israel/USA", [
-    #     "Led a team of 5 engineers in the design and development of DSLAM systems, INTERCARD communication BSP for vxWorks, and various hardware drivers.",
-    #     "Delivered customer-tailored systems, some of which are still in operation.",
-    #     "Conducted employee training programs."
-    # ], ['C', "Linux kernel", "Bare metal"], False),
-    # ("1995-2000: Software Engineer - TDSoft", [
-    #     "Developed infrastructures and low-level drivers for embedded real-time systems in C, C++, and Java.",
-    #     "Participated in several development projects, including a Windows management system for strategic customers such as Deutsche Telekom and ECI.",
-    #     "Designed and implemented significant parts of a V5 Class 5 switch (LE) simulator with ISDN support and developed GUI management for the simulator."
-    # ], ['C/C++', "Java", "Bare metal", "RTKernel", "pSOS"], False)
 ]
+
+if LONG_VERSION:
+    experience += [
+        ("2005-2007: Software Engineer - Broadlight Ltd.", [
+            "Contributed to the development of the world's first fully integrated GPON chip.",
+            "Supported the hardware team in post-production verification and pre-fab processes.",
+            "Prepared firmware for design verification, post-production chip verification, and a series of drivers."
+        ], ['C', 'Verilog', "Linux kernel", "Specman", "Bare metal"], True),
+        ("2001-2004: Software Engineer - Terayon Israel/USA", [
+            "Led a team of 5 engineers in the design and development of DSLAM systems, INTERCARD communication BSP for vxWorks, and various hardware drivers.",
+            "Delivered customer-tailored systems, some of which are still in operation.",
+            "Conducted employee training programs."
+        ], ['C', "Linux kernel", "Bare metal"], False),
+        ("1995-2000: Software Engineer - TDSoft", [
+            "Developed infrastructures and low-level drivers for embedded real-time systems in C, C++, and Java.",
+            "Participated in several development projects, including a Windows management system for strategic customers such as Deutsche Telekom and ECI.",
+            "Designed and implemented significant parts of a V5 Class 5 switch (LE) simulator with ISDN support and developed GUI management for the simulator."
+        ], ['C/C++', "Java", "Bare metal", "RTKernel", "pSOS"], False)
+    ]
+
 
 for position, details, technologies, page_break in experience:
     doc.add_heading(position, level=2)
